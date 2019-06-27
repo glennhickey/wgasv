d983a938ec88420308ad6dbd52c709f098fa6b8c
Glenn Hickey <glenn.hickey@gmail.com>
2019-06-27 10:10:33 -0400

    specify chrom

--- hgsvc/cactus-input.sh
+++ hgsvc/cactus-input.sh
@@ -6,9 +6,10 @@
 usage() {
     # Print usage to stderr                                                                                                                                                          
     exec 1>&2
-    printf "Usage: $0 [OPTIONS] \n"
-         printf "Arguments:\n"
-         printf "Options:\n"
+    printf "Usage: $0 [OPTIONS] <CHROM>\n"
+    printf "Arguments:\n"
+	 printf "   CHROM: Name of chromosome. ex chr21\n"
+    printf "Options:\n"
     exit 1
 }
 
@@ -22,18 +23,20 @@ done
 
 shift $((OPTIND-1))
 
-if [[ "$#" -lt "0" ]]; then
+if [[ "$#" -lt "1" ]]; then
     # Too few arguments
     usage
 fi
 
+CHROM="${1}"
+shift
 
 echo "((((HG00514.0:0.01,HG00514.1:0.01):0.02,(HG00733.0:0.01,HG00733.1:0.01):0.02):0.02,(NA19240.0:0.01,NA19240.1:0.01):0.04):0.04,hg38:0.01)"
-echo "hg38 $(pwd)/hg38_chr21.fa"
-echo "HG00514.0 $(pwd)/HG00514_chr21_0.fa"
-echo "HG00514.1 $(pwd)/HG00514_chr21_1.fa"
-echo "HG00733.0 $(pwd)/HG00533_chr21_0.fa"
-echo "HG00733.1 $(pwd)/HG00533_chr21_1.fa"
-echo "NA19240.0 $(pwd)/NA19240_chr21_0.fa"
-echo "NA19240.1 $(pwd)/NA19240_chr21_1.fa"
+echo "hg38 $(pwd)/hg38_${CHROM}.fa"
+echo "HG00514.0 $(pwd)/HG00514_${CHROM}_0.fa"
+echo "HG00514.1 $(pwd)/HG00514_${CHROM}_1.fa"
+echo "HG00733.0 $(pwd)/HG00533_${CHROM}_0.fa"
+echo "HG00733.1 $(pwd)/HG00533_${CHROM}_1.fa"
+echo "NA19240.0 $(pwd)/NA19240_${CHROM}_0.fa"
+echo "NA19240.1 $(pwd)/NA19240_${CHROM}_1.fa"
 
