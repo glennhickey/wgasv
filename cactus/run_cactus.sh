31696c0c383d3ed7bcd02c73561c9d9fb015e469
Ubuntu <ubuntu@ip-172-31-6-183.us-west-2.compute.internal>
2019-07-03 15:19:41 +0000

    cactus script

--- cactus/run_cactus.sh
+++ cactus/run_cactus.sh
@@ -6,19 +6,26 @@
 # need to source /ebs1/cactus/cactus_env/bin/activate first
 #
 
+CONFIG=0
+
 usage() {
     # Print usage to stderr
     exec 1>&2
-    printf "Usage: $0 [OPTIONS] <SEQFILE> <HALFILE>\n"
+    printf "Usage: $0 [OPTIONS] <SEQFILE> <HALFILE> <REFNAME>\n"
 	 printf "Arguments:\n"
 	 printf "   SEQFILE: Cactus seqfile containing seqfile\n"
-	 printf "   HALFILE: \n"
+	 printf "   HALFILE: Output\n"
+	 printf "   REFNAME: Reference genome\n"
 	 printf "Options:\n"
+	 printf "  -c FILE:  Config file\n" 
     exit 1
 }
 
-while getopts "h" o; do
+while getopts "hc:" o; do
     case "${o}" in
+	c)
+	    CONFIG=${OPTARG}
+	    ;;
         *)
             usage
             ;;
@@ -29,7 +36,7 @@ shift $((OPTIND-1))
 
 printf "Need to run in toil-vg virtualenv with vg's .source_me.sh sourced\n"
 
-if [[ "$#" -lt "2" ]]; then
+if [[ "$#" -lt "3" ]]; then
     # Too few arguments
     usage
 fi
@@ -40,10 +47,28 @@ SEQFILE="${1}"
 shift
 HALFILE="${1}"
 shift
+REFNAME="${1}"
+shift
+
+set -x
+
+OPTS=""
+if [ $CONFIG != 0 ]
+then
+    OPTS="--config ${CONFIG}"
+fi
 
 if [ -f "${HALFILE}" ]
 then
     echo "hal exists, not re-rerunning cactus"
 else
-    /usr/bin/time -v cactus jobstore_${HALFILE} ${SEQFILE} ${HALFILE}
+    /usr/bin/time -v cactus jobstore_${HALFILE} ${SEQFILE} ${HALFILE} ${OPTS} 2> TIME.${HALFILE}
+fi
+
+VGFILE="$(basename "${HALFILE}" .hal).vg"
+if [ -f "${VGFILE}" ]
+then 
+    echo "vg exits, skipping hal2vg"
+else
+    /usr/bin/time -v hal2vg ${HALFILE} --refGenome ${REFNAME} --noAncestors > ${VGFILE} 2> TIME.${VGFILE}
 fi
